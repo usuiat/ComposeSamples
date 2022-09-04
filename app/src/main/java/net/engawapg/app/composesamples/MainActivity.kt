020dfb4de41bcecbd2b9c1134615ad5a65ab3ed1
usuiat <stripewani@gmail.com>
2022-09-04 12:59:43 +0900

    Material3のListItemを使う。

--- app/src/main/java/net/engawapg/app/composesamples/MainActivity.kt
+++ app/src/main/java/net/engawapg/app/composesamples/MainActivity.kt
@@ -5,23 +5,15 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -106,26 +98,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Samples(samples: List<Sample> = sampleList, onSelectSample: (String)->Unit) {
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(2.dp),
-        modifier = Modifier.background(MaterialTheme.colorScheme.surfaceVariant),
-    ) {
+    LazyColumn {
         items(samples) { sample ->
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .fillParentMaxWidth()
-                    .height(80.dp)
-                    .background(MaterialTheme.colorScheme.surface)
-                    .clickable { onSelectSample(sample.route) }
-            ) {
-                Text(
-                    text = stringResource(id = sample.titleResource),
-                    style = MaterialTheme.typography.titleLarge,
-                )
-            }
+            ListItem(
+                headlineText = { Text(stringResource(id = sample.titleResource)) },
+                modifier = Modifier.clickable { onSelectSample(sample.route) }
+            )
+            Divider()
         }
     }
 }
